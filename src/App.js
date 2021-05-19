6e34e3e40abf9c4d3a95a231eb7ca77e5167d082
Zirach Hangad <zirachdev@gmail.com>
2021-05-19 23:16:43 +0800

    Adding another line for testing purposese

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>PORTFOLIO</h1>
+      <h2>A little about myself</h2>
     </div>
   );
 }
